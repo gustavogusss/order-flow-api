d2637468b8385a053c41f7f3f9642aed018ed487
Gustavo Metzler Pontes <gustavo_metzler@hotmail.com>
2026-05-26 09:16:06 -0300

    Initial Commit

---
+++ api/src/test/java/com/order_flow/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order_flow.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
